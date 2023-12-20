95fd4947ae34833256805924485478a7358d9c87
Cjr37 <carlosclase37@gmail.com>
2023-12-20 18:03:39 +0100

    Funciona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {DataService} from "./services/data.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ObservableSimpleAngular';
+export class AppComponent implements OnInit{
+  posts$: Observable<any[]>
+
+  constructor(private dataService : DataService) {
+  }
+
+  ngOnInit() {
+    this.posts$ = this.dataService.getPosts()
+  }
 }
